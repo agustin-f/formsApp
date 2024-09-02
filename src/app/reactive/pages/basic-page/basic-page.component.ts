3aff88a99624f205c4d7e1ad41c4ab41d3c69422
Agustin Farisano <farisanoagustin@gmail.com>
2024-09-02 16:23:05 -0300

    reactive inputs form valitations

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./basic-page.component.css']
 })
 export class BasicPageComponent {
+
+ public myForm:FormGroup = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      price: new FormControl(0, [Validators.required, Validators.min(0)]),
+      inStorage: new FormControl(0, [Validators.required, Validators.min(0)]),
+ })
+
+
+ onSave(){
+  console.log(this.myForm.value);
+ }
 
 }
